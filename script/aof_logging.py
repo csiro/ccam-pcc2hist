7224dd325b3a8754777283798354b752ba5847e7
Benn, David (IM&T, Waite Campus) <David.Benn@csiro.au>
2015-08-04 03:27:16 +0000

    ASCTM-247: fixed logging script

--- script/aof_logging.py
+++ script/aof_logging.py
@@ -11,23 +11,39 @@ if len(sys.argv) < 2:
     sys.exit(1)
 
 func_start = re.compile('\s*function\s+(\w+)\s*\(')
+func_assign = re.compile('ncf90_\w+\s*=')
 func_end = re.compile('\s*end\s+function\s+(\w+)')
 
 f = open(sys.argv[1], "r")
 lines = f.readlines()
 f.close()
 
+state = "SEARCH_FUNC_START"
+
 for line in lines:
-    start_match = func_start.search(line)
-    if start_match is not None:
-        print line.rstrip()
-        print '        print *, ">> Entering {0}"'.format(start_match.group(1))
-    else:
+    line = line.rstrip()
+
+    if state == "SEARCH_FUNC_START":
+        start_match = func_start.search(line)
+        if start_match is not None:
+            # Skip until past declarations
+            state = "SKIP_DECL"
+        print line
+    elif state == "SKIP_DECL":
+        if func_assign.search(line) is not None or \
+                line.find('present') != -1 or \
+                line.find('=>') != -1: 
+            print '        print *, ' \
+                '">> Entering {0}"\n'.format(start_match.group(1))
+            state = "SEARCH_FUNC_END"
+        print line
+    elif state == "SEARCH_FUNC_END":
         end_match = func_end.search(line)
         if end_match is not None:
-            print '        print *, ">> Exiting {0}"'.format(end_match.group(1))
-            
-        print line.rstrip()
+            print '        print *, ' \
+                '">> Exiting {0}"\n'.format(end_match.group(1))
+            state = "SEARCH_FUNC_START"
+        print line
 
 
     
