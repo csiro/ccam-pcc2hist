39db43bf0ffeb0c05c19b144df355277e2f86c7c
tha051 <tha051@localhost>
2021-12-21 20:29:23 +0000

    Reduce barrier in pcc2hist to once every 24 time-steps.  Should be enough to prevent mpi buffers from overflowing on some systems

--- history.f90
+++ history.f90
@@ -995,7 +995,7 @@ contains
       end if  
 
       do ifld = 1,totflds
-         if ( histinfo(ifld)%used .and. myid == histinfo(ifld)%procid ) then
+         if ( histinfo(ifld)%used .and. myid==histinfo(ifld)%procid ) then
             ncid = histinfo(ifld)%ncid
             dims = histdims(ifld)
             call create_ncvar(histinfo(ifld), ncid, dims)
@@ -2044,6 +2044,8 @@ contains
       integer :: istart, iend
       integer :: ave_type, nlev, count, ncid
       integer :: k
+      integer, save :: barrier_counter = 0
+      integer, parameter :: barrier_max = 24
       real :: umin, umax, addoff, sf
 !     Temporary for interpolated output
       integer, dimension(:), allocatable, save :: k_indx
@@ -2401,9 +2403,13 @@ contains
 
       end do ! Loop over fields
       
-      call START_LOG(mpibarrier_begin)
-      call MPI_Barrier(comm_world,ierr) ! avoids crashes on some systems      
-      call END_LOG(mpibarrier_end)
+      barrier_counter = barrier_counter + 1
+      if ( barrier_counter >= barrier_max ) then
+        barrier_counter = 0  
+        call START_LOG(mpibarrier_begin)
+        call MPI_Barrier(comm_world,ierr) ! avoids crashes on some systems      
+        call END_LOG(mpibarrier_end)
+      end if  
 
       deallocate(hist_a)
 #else
@@ -2543,9 +2549,13 @@ contains
 
       end do ! Loop over fields
 
-      call START_LOG(mpibarrier_begin)
-      call MPI_Barrier(comm_world,ierr) ! avoids crashes on some systems      
-      call END_LOG(mpibarrier_end)
+      barrier_counter = barrier_counter + 1
+      if ( barrier_counter >= barrier_max ) then
+        barrier_counter = 0  
+        call START_LOG(mpibarrier_begin)
+        call MPI_Barrier(comm_world,ierr) ! avoids crashes on some systems      
+        call END_LOG(mpibarrier_end)
+      end if  
       
       deallocate( hist_a, hist_g )
 #endif
