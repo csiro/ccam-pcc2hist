f7de6963f65e0d2ab7909b442990035589886598
Thatcher, Marcus (O&A Aspendale) <Marcus.Thatcher@csiro.au>
2022-07-27 12:25:01 +1000

    Updated makefile for pcc2hist version

--- revision.h
+++ revision.h
@@ -1,1 +1,1 @@
-   character(len=*), parameter :: cc2hist_revision='SVN-r5252:5278M'
+   character(len=*), parameter :: cc2hist_revision='GIT-Date:   Wed Jul 27 10:02:47 2022 +1000 commit e7d86b88f9ab67df4e1dc5d8d08c715b563a1d0e'
