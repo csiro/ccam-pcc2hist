b4ffffb21364ba515005940c50a99ebdfa95db8e
Marcus Thatcher <mthatcher@setonix-01.pawsey.org.au>
2022-08-11 12:36:45 +0800

    Add safe to pcc2hist with SETONIX

--- revision.h
+++ revision.h
@@ -1,2 +1,2 @@
 character(len=*), parameter :: cc2hist_revision= &
-'GIT-Date:   Wed Aug 10 09:40:11 2022 +0800 commit fde7adecb4cba78ee8fb1b7ea4e55f6da953ecf5'
+'GIT-Date:   Thu Aug 11 04:22:48 2022 +0800 commit b68fae58378ab51edc6567cd7537bb0fd2f4462f'
